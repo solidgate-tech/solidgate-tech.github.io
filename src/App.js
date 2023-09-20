d458c0cee3a3f3197aa478b4b888ff9711df332b
Max Baginskiy <max.baginskiy@solidgate.com>
2023-09-20 10:29:11 +0300

    fix add few more items

--- src/App.js
+++ src/App.js
@@ -483,6 +483,14 @@ function TechRadar() {
                 //  1 = moved in  (triangle pointing up)
             },
             {
+                label: "Blue-green deploy",
+                quadrant: 3,          // 0,1,2,3 (counting clockwise, starting from bottom right)
+                ring: 2,              // 0,1,2,3 (starting from inside)
+                moved: 0             // -1 = moved out (triangle pointing down)
+                //  0 = not moved (circle)
+                //  1 = moved in  (triangle pointing up)
+            },
+            {
                 label: "GitOps",
                 quadrant: 3,          // 0,1,2,3 (counting clockwise, starting from bottom right)
                 ring: 1,              // 0,1,2,3 (starting from inside)
