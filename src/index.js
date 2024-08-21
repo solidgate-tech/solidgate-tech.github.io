5ea92ab99ebe1ec67fde80141a2b5e24f25498b3
max.baginskiy <max.baginskiy@solidgate.com>
2024-08-21 15:51:36 +0300

    add 2024

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { MemoisedTechRadar, MemoisedFooter } from './App';
+import { MemoisedFooter, MemoisedTechRadar2024 } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MemoisedTechRadar />
+   <MemoisedTechRadar2024 />
   </React.StrictMode>
 );
 
