88defe6640b8ef65dcf06b2f6fa13e16744ebca9
Nikita Popov <nikita.popov@solidgate.com>
2025-07-14 22:18:31 +0300

    chore: fixes and clean-ups

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { MemoisedFooter, MemoisedApp } from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <MemoisedApp />
+    <App />
   </React.StrictMode>
 );
-
-const footer = ReactDOM.createRoot(document.getElementById('footer'));
-footer.render(
-    <React.StrictMode>
-        <MemoisedFooter />
-    </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
